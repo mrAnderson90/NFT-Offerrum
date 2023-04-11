ce92063179beb45df407e345e05ea7f6e4e0a3cb
mrAnderson90 <tyler.andreev@mail.ru>
2023-04-11 17:31:59 +0300

    Add main structure of project

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const { parallel, src, dest, watch} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const pug = require('gulp-pug');
+
+const buildSass = () => {
+	console.log('Компиляция Scss');
+	return src('app/scss/app.scss')
+	.pipe(sass())
+	.pipe(dest('styles/'));
+};
+
+const buildPug = () => {
+	console.log('Компиляция Pug');
+	return src('app/pages/*.pug')
+	.pipe(pug({
+		pretty: true
+	}))
+	.pipe(dest('./'));
+};
+
+const watchers = () => {
+	watch('app/scss/**/*.scss', buildSass);
+	watch('app/pages/**/*.pug', buildPug);
+}
+
+exports.build = parallel(buildSass, buildPug);
+exports.watchers = watchers;
