294f1f37e9dea07a4f790026d91abaf628c00d77
mrAnderson90 <tyler.andreev@mail.ru>
2023-04-17 14:03:33 +0300

    Add subscribe form

---
+++ README.md
@@ -0,0 +1,15 @@
+# NFT Project
+
+## _Follow the link to see deploy:_
+
+[Main page]()
+
+## The following technologies were used:
+- HTML5
+- CSS3
+- Flex
+- Grid
+- Pug
+- SASS
+- Gulp
+---
--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,17 @@
+// -- Modal Menu -- //
 // Open Modal Menu
 document.getElementById("open-modal-menu")
 	.addEventListener("click", () => {
 		document.getElementById("modal-menu")
 			.classList.add("active")
-	});
+});
 
 // Close Modal Menu
 document.getElementById("close-modal-menu")
 	.addEventListener("click", () => {
 		document.getElementById("modal-menu")
 			.classList.remove("active");
-	});
+});
 
 // Close Modal Menu by press to Esc
 window.addEventListener("keydown", (e) => {
@@ -18,4 +19,34 @@ window.addEventListener("keydown", (e) => {
 		document.getElementById("modal-menu")
 			.classList.remove("active");
 		}
-	});
+});
+
+// -- Modal Subscribe -- //
+// Open Subscribe form
+document.getElementById("subscribe-open")
+	.addEventListener("click", () => {
+		document.getElementById("subscribe-modal")
+		.classList.add("active")
+});
+
+// Close Subscribe form
+document.getElementById("subscribe-close")
+	.addEventListener("click", () => {
+		document.getElementById("subscribe-modal")
+		.classList.remove("active")
+});
+
+// Close Subscribe form by press to Esc
+window.addEventListener("keydown", (e) => {
+	if (e.key === "Escape") {
+		document.getElementById("subscribe-modal")
+		.classList.remove("active")
+	}
+});
+
+// Close Subscribe form by click outside
+document.getElementById("subscribe-modal")
+	.addEventListener("click", event => {
+		if (event._isClickWithInModal) return;
+		event.currentTarget.classList.remove("active");
+});
